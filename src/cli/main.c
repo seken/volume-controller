f07d5692ad8d32e24b9c0605cea4da1bd63b901d
Mark Goodall <markit@seken.co.uk>
2011-10-21 21:42:43 +0100

    Added watchdog options to cli program

--- src/cli/main.c
+++ src/cli/main.c
@@ -16,6 +16,8 @@ static struct option long_options[] = {
 	{"watchdog", no_argument, 0, 'w'},
 	{"volume", required_argument, 0, 'v'},
 	{"device", required_argument, 0, 'd'},
+	{"disable-watchdog", required_argument, 0, 'l'},
+	{"enable-watchdog", required_argument, 0, 'm'},
 };
 
 const char *package_string = PACKAGE_STRING;
@@ -23,11 +25,13 @@ const char *package_string = PACKAGE_STRING;
 void usage() {
 	fprintf(stderr, "%s\n\n", package_string);
 	fprintf(stderr, "Usage:\n");
-	fprintf(stderr, "  --help               This message\n");
-	fprintf(stderr, "  -d, --device=NAME    The device name to communicate with\n");
-	fprintf(stderr, "  -w, --watchdog       Keep the device on (default)\n");
-	fprintf(stderr, "  -v, --volume=NUM     Set the volume level (0-63)\n");
-	fprintf(stderr, "  -o, --off            Turn the device off\n");
+	fprintf(stderr, "  --help                  This message\n");
+	fprintf(stderr, "  -d, --device=NAME       The device name to communicate with\n");
+	fprintf(stderr, "  -w, --watchdog          Keep the device on (default)\n");
+	fprintf(stderr, "  -v, --volume=NUM        Set the volume level (0-63)\n");
+	fprintf(stderr, "  -o, --off               Turn the device off\n");
+	fprintf(stderr, "  -l, --disable-watchdog  Keep the device on\n");
+	fprintf(stderr, "  -m, --enable-watchdog   Allow the device to turn off\n");
 	exit(-2);
 }
 
@@ -38,10 +42,12 @@ int main(int argc, char **argv) {
 	int volume = 0;
 	int volume_set = 0;
 	int watch_dog = 1;
+	int enable_watchdog = 0;
+	int disable_watchdog = 0;
 
 	while (1) {
 		int option_index = 0;
-		int c = getopt_long(argc, argv, "hd:v:o", long_options, &option_index);
+		int c = getopt_long(argc, argv, "hd:v:olmw", long_options, &option_index);
 
 		if (c == -1) {
 			break;
@@ -70,6 +76,15 @@ int main(int argc, char **argv) {
 				volume_set = 1;
 				volume = atoi(optarg);
 				watch_dog = 0;
+				break;
+
+			case 'l':
+				watch_dog = 1;
+				disable_watchdog = 1;
+				break;
+			case 'm':
+				watch_dog = 1;
+				enable_watchdog = 1;
 				break;
 
 			default:
@@ -106,6 +121,14 @@ int main(int argc, char **argv) {
 	} else if (volume_set) {
 		const char message[512] = {0};
 		sprintf(message, "v%d\n", volume);
+		result = write(dev_handle, &message, strlen(message));
+	}
+
+	if (enable_watchdog) {
+		const char message[] = "i\n";
+		result = write(dev_handle, &message, strlen(message));
+	} else if (disable_watchdog) {
+		const char message[] = "o\n";
 		result = write(dev_handle, &message, strlen(message));
 	}
 
